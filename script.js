a3f66c6d23a5ac4ec16780f223f872641dcf278f
EmmaKespo <kespoemma@gmail.com>
2026-02-04 13:50:18 -0600

    play for 5 round

--- script.js
+++ script.js
@@ -35,3 +35,44 @@ function playRound(humanChoice, computerChoice) {
         return "Computer wins!";
     }
 }
+function playgame() {
+    let humanscore = 0;
+    let computerscore = 0;
+    function getComputerChoice() {
+        const randomNumber = Math.random(); 
+        if (randomNumber < 0.33) {
+            return 'rock';
+    } else if (randomNumber <= 0.66) {
+            return 'paper';
+    } else {
+            return 'scissors';
+    }
+    }
+    function getHumanChoice() {
+        return prompt("Do you choose rock, paper or scissors?");
+    }
+    if (humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice === "paper") {
+            console.log("You win this round!");
+            humanscore++;
+    }else console.log("Computer wins this round!");
+            computerscore++;
+    }
+for (let round = 1; round <= 5; round++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    console.log(`Round ${round}:`);
+    playRound(humanChoice, computerChoice);
+    console.log(`Score - You: ${humanscore}, Computer: ${computerscore}`);
+}if (humanscore > computerscore) {
+    console.log("Congratulations! You won the game!");
+} else if (computerscore > humanscore) {
+    console.log("Computer wins the game! Better luck next time.");
+}else {
+    console.log("The game is a tie!");
+}
+playgame();
+function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
